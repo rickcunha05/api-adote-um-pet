564192d03339961cd428119d718b2363c642b95b
rickcunha05 <henrique.cunha5@outlook.com>
2022-06-26 23:38:26 -0300

    first commit

---
+++ src/main/java/br/com/treinaweb/adoteumpet/core/models/Pet.java
@@ -0,0 +1,34 @@
+package br.com.treinaweb.adoteumpet.core.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pet {
+
+        @Id        
+        @EqualsAndHashCode.Include
+        @GeneratedValue(strategy = GenerationType.IDENTITY)        
+        private Long id;
+
+        @Column(nullable = false)
+        private String nome;
+
+        @Column(nullable = false)
+        private String historia;
+
+        @Column(nullable = false)
+        private String foto;
+}
---
+++ src/main/java/br/com/treinaweb/adoteumpet/core/repositories/AdocaoRepository.java
@@ -0,0 +1,23 @@
+package br.com.treinaweb.adoteumpet.core.repositories;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.treinaweb.adoteumpet.core.models.Pet;
+
+public interface AdocaoRepository  extends JpaRepository<Pet, Long>{
+    
+    default Pet findByIdOrElseThrow(Long id) {
+
+            var petOptional = findById(id);
+
+            if (petOptional.isPresent()){ 
+                return petOptional.get();
+            }
+            throw new EntityNotFoundException("Pet Não encontrado!");
+            
+    }
+
+    
+}
